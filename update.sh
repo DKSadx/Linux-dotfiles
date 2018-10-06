b27ff6a91cc3c529aec9a4bbe28f489d304c34e3
DKSadx <sadx.carmon@gmail.com>
2018-10-06 16:12:14 +0200

    script for updating dotfiles

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -r ~/Documents/backup/dotfiles/i3
+rm -r ~/Documents/backup/dotfiles/polybar
+rm -r ~/Documents/backup/dotfiles/rofi
+rm -r ~/Documents/backup/dotfiles/sublime-text-3
+rm -r ~/Documents/backup/dotfiles/My_scripts
+rm -r ~/Documents/backup/dotfiles/compton
+
+mkdir ~/Documents/backup/dotfiles/compton
+mkdir ~/Documents/backup/dotfiles/sublime-text-3
+cp -r ~/.config/i3 ~/Documents/backup/dotfiles/
+cp -r ~/.config/polybar ~/Documents/backup/dotfiles/
+cp -r ~/.config/rofi ~/Documents/backup/dotfiles/
+cp -r ~/.config/sublime-text-3/"Installed Packages" ~/Documents/backup/dotfiles/sublime-text-3
+cp -r ~/.config/sublime-text-3/"Packages" ~/Documents/backup/dotfiles/sublime-text-3
+cp -r ~/My_scripts ~/Documents/backup/dotfiles/
+cp ~/.config/compton.conf ~/Documents/backup/dotfiles/compton
+cp ~/.bashrc ~/Documents/backup/dotfiles/
+cp ~/.bash_profile ~/Documents/backup/dotfiles/
+cp ~/.Xdefaults ~/Documents/backup/dotfiles/
