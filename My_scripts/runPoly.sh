ffbdbdbcb02bb19c43701a8636a538f43d9f31a8
DKSadx <sadx.carmon@gmail.com>
2018-11-19 14:57:19 +0000

    better script for launching polybar

--- My_scripts/runPoly.sh
+++ My_scripts/runPoly.sh
@@ -1,12 +1,10 @@
-
 #!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-# polybar example &
-polybar example -r & 
+# Launch polybar
+polybar example -r &
