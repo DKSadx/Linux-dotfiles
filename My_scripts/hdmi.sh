fa5f18d27e5712ba0e89a319ebc152dfc2a0cbdb
DKSadx <sadx.carmon@gmail.com>
2018-11-19 14:52:43 +0000

    hdmi script for Arch linux

--- My_scripts/hdmi.sh
+++ My_scripts/hdmi.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Gets the state of hdmi(connected/disconnected)
-display="$(xrandr | grep -i "hdmi" | cut -d"(" -f1)"
+display="$(xrandr | grep -i "hdmi" | cut -d"(" -f1 | cut -d" " -f2 | head -n1)"
 # Checks if the string equals "HDMI-0 connected "
-if [ "$display" == "HDMI-0 connected " ]; then
+if [ "$display" == "connected" ]; then
 	# Sets output to HDMI
-	xrandr --output LVDS-0 --auto --output HDMI-0 --auto --right-of LVDS-0
+	xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1
 	# Refresh the audio states
         pulseaudio -k
 	# Sets Audiocard profile to HDMI-surround
-	pactl set-card-profile 0 output:hdmi-surround-extra1
+	pactl set-card-profile 0 output:hdmi-stereo
 
 else
 	# Turns off HDMI output
-	xrandr --output HDMI-0 --off
+	xrandr --output HDMI1 --off
 	# Turns off HDMI audio output
-	pactl set-card-profile 0 off
+	pactl set-card-profile 0 output:analog-stereo
 fi
 
 # Check video cards:
