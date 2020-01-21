030e66fd59c96c75fc321783dbf0a7cda35b4406
DKSadx <sadx.carmon@gmail.com>
2020-01-21 14:07:54 +0000

    Dotfiles are hard links now

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cp -r ~/.config/i3/* ~/Documents/backup/Linux-dotfiles/i3/
-cp -r ~/.config/polybar/* ~/Documents/backup/Linux-dotfiles/polybar/
-cp -r ~/.config/ranger/* ~/Documents/backup/Linux-dotfiles/ranger/
-cp -r ~/.config/rofi/* ~/Documents/backup/Linux-dotfiles/rofi/
-cp -r ~/.config/dunst/* ~/Documents/backup/Linux-dotfiles/dunst/
-cp ~/.config/compton.conf ~/Documents/backup/Linux-dotfiles/compton/
-cp ~/.bashrc ~/Documents/backup/Linux-dotfiles/bash/
-cp ~/.bash_profile ~/Documents/backup/Linux-dotfiles/bash/
-cp ~/.zshrc ~/Documents/backup/Linux-dotfiles/zsh/
-cp ~/.vimrc ~/Documents/backup/Linux-dotfiles/vim/
-cp ~/.tmux.conf ~/Documents/backup/Linux-dotfiles/tmux/
-cp ~/.xinitrc ~/Documents/backup/Linux-dotfiles/
-cp ~/.Xmodmap ~/Documents/backup/Linux-dotfiles/
-cp ~/.Xresources ~/Documents/backup/Linux-dotfiles/
+#cp -r ~/.config/i3/* ~/Documents/backup/Linux-dotfiles/i3/
+#cp -r ~/.config/polybar/* ~/Documents/backup/Linux-dotfiles/polybar/
+#cp -r ~/.config/ranger/* ~/Documents/backup/Linux-dotfiles/ranger/
+#cp -r ~/.config/rofi/* ~/Documents/backup/Linux-dotfiles/rofi/
+#cp -r ~/.config/dunst/* ~/Documents/backup/Linux-dotfiles/dunst/
+#cp ~/.config/compton.conf ~/Documents/backup/Linux-dotfiles/compton/
+#cp ~/.bashrc ~/Documents/backup/Linux-dotfiles/bash/
+#cp ~/.bash_profile ~/Documents/backup/Linux-dotfiles/bash/
+#cp ~/.zshrc ~/Documents/backup/Linux-dotfiles/zsh/
+#cp ~/.vimrc ~/Documents/backup/Linux-dotfiles/vim/
+#cp ~/.tmux.conf ~/Documents/backup/Linux-dotfiles/tmux/
+#cp ~/.xinitrc ~/Documents/backup/Linux-dotfiles/
+#cp ~/.Xmodmap ~/Documents/backup/Linux-dotfiles/
+#cp ~/.Xresources ~/Documents/backup/Linux-dotfiles/
 cp ~/My_scripts/*.sh ~/Documents/backup/Linux-dotfiles/My_scripts/
